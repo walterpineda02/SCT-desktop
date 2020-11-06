ce581ec443ee544c7a484b4dc43cad9f9058c1e1
Juan Sandino <jsandino@aimservices.tech>
2020-11-06 12:46:07 -0600

    chore; add ORM and Diagram

---
+++ SCT/ORMCode/Archivo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace SCT.sct
+{
+
+    public partial class Archivo
+    {
+        public Archivo(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ SCT/ORMCode/DetalleReparacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace SCT.sct
+{
+
+    public partial class DetalleReparacion
+    {
+        public DetalleReparacion(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
